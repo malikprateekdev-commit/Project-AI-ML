3b30f232ba888d000d65763b306b063d302bdf57
malikprateekdev-commit <malikprateek.dev@gmail.com>
2026-08-25 21:17:01 +0530

    Define command-line arguments in get_input_args function
    Updated the get_input_args function to define command-line arguments for image folder, CNN architecture, and dog names file with default values.

--- intropyproject-classify-pet-images/get_input_args.py
+++ intropyproject-classify-pet-images/get_input_args.py
@@ -1,43 +1,65 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 # */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
-#                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                   
-# REVISED DATE: 
-# PURPOSE: Create a function that retrieves the following 3 command line inputs 
-#          from the user using the Argparse Python module. If the user fails to 
-#          provide some or all of the 3 inputs, then the default values are
-#          used for the missing inputs. Command Line Arguments:
-#     1. Image Folder as --dir with default value 'pet_images'
-#     2. CNN Model Architecture as --arch with default value 'vgg'
-#     3. Text File with Dog Names as --dogfile with default value 'dognames.txt'
+
+# PROGRAMMER: Prateek Malik
+# DATE CREATED: 25 August 2026
+# REVISED DATE:
+
+# PURPOSE: Create a function that retrieves the following three command-line
+#          inputs using the argparse module. Default values are used when
+#          arguments are not provided.
 #
-##
-# Imports python modules
+# Command-line arguments:
+#   1. Image folder: --dir
+#   2. CNN model architecture: --arch
+#   3. Dog names text file: --dogfile
+
+
+# Import Python modules.
 import argparse
 
-# TODO 1: Define get_input_args function below please be certain to replace None
-#       in the return statement with parser.parse_args() parsed argument 
-#       collection that you created with this function
-# 
+
 def get_input_args():
     """
-    Retrieves and parses the 3 command line arguments provided by the user when
-    they run the program from a terminal window. This function uses Python's 
-    argparse module to created and defined these 3 command line arguments. If 
-    the user fails to provide some or all of the 3 arguments, then the default 
-    values are used for the missing arguments. 
-    Command Line Arguments:
-      1. Image Folder as --dir with default value 'pet_images'
-      2. CNN Model Architecture as --arch with default value 'vgg'
-      3. Text File with Dog Names as --dogfile with default value 'dognames.txt'
-    This function returns these arguments as an ArgumentParser object.
-    Parameters:
-     None - simply using argparse module to create & store command line arguments
+    Retrieves and parses three command-line arguments:
+
+    1. Image folder as --dir, defaulting to 'pet_images/'.
+    2. CNN architecture as --arch, defaulting to 'vgg'.
+    3. Dog names file as --dogfile, defaulting to 'dognames.txt'.
+
     Returns:
-     parse_args() -data structure that stores the command line arguments object  
+        argparse.Namespace: Parsed command-line arguments.
     """
-    # Replace None with parser.parse_args() parsed argument collection that 
-    # you created with this function 
-    return None
+
+    # Create an argument parser.
+    parser = argparse.ArgumentParser()
+
+    # Add the pet images directory argument.
+    parser.add_argument(
+        "--dir",
+        type=str,
+        default="pet_images/",
+        help="Path to the folder containing pet images."
+    )
+
+    # Add the CNN model architecture argument.
+    parser.add_argument(
+        "--arch",
+        type=str,
+        default="vgg",
+        choices=["resnet", "alexnet", "vgg"],
+        help="CNN model architecture: resnet, alexnet, or vgg."
+    )
+
+    # Add the dog names file argument.
+    parser.add_argument(
+        "--dogfile",
+        type=str,
+        default="dognames.txt",
+        help="Path to the text file containing dog names."
+    )
+
+    # Parse and return all command-line arguments.
+    return parser.parse_args()
